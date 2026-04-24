dfe80f06595d5d52004d04e5a22eb78be1810bf5
Yan Alexandre <alexandreyan2006@gmail.com>
2026-04-24 10:37:20 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/fatorial.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned long long fatorial_recursivo(int n){
+    int resultado = 1;
+    if(n == 0){
+        return resultado;
+    } else {
+        resultado = n * fatorial_recursivo(n-1); // O(n)
+    }
+    return resultado;
+}
+
+long long fatorial_iterativo(int n) {
+    long long res = 1;
+    for (int i = 1; i <= n; i++) {
+        res *= i; // O(n)
+    }
+    return res;
+}
+
+int main(){
+
+    int res = 0;
+
+    clock_t inicio = clock();
+    res = fatorial_recursivo(5);
+    clock_t final = clock();
+
+    double duracao = (double)(final - inicio)/ CLOCKS_PER_SEC;
+    printf("O fatoria deu %llu em %.2f\n", res, duracao);
+
+    inicio = clock();
+    res = fatorial_recursivo(25);
+    final = clock();
+
+    duracao = (double)(final - inicio)/ CLOCKS_PER_SEC;
+    printf("O fatoria deu %llu em %f\n", res, duracao);
+
+    inicio = clock();
+    res = fatorial_iterativo(25);
+    final = clock();
+
+    duracao = (double)(final - inicio)/ CLOCKS_PER_SEC;
+    printf("O fatoria deu %llu em %f\n", res, duracao);
+
+
+    return 0;
+}
---
+++ praticas/pratica02/selecao.h
@@ -0,0 +1,8 @@
+#ifndef SELECAO_H
+#define SELECAO_H
+
+int buscar_sequencial(int v[],int tam, int valor);
+void troca(int *a,int *b);
+
+
+#endif
